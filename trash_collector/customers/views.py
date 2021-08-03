b9ee9e4ed24240cd26bcef39d37982b54436ba67
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2021-08-03 13:15:59 -0400

    refactored create method, left commented out for now

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -33,12 +33,12 @@ def create(request):
         address = request.POST.get('address')
         zipcode = request.POST.get('zipcode')
         weekly_pickup_day = request.POST.get('weekly_pickup_day')
-        one_time_pickup = request.POST.get('one_time_pickup')
+        # one_time_pickup = request.POST.get('one_time_pickup')
         balance = request.POST.get('balance')
-        suspend_start = request.POST.get('suspend_start')
-        suspend_end = request.POST.get('suspend_end')
-        new_customer = Customer(name = name, address = address, zipcode = zipcode, weekly_pickup_day = weekly_pickup_day, one_time_pickup = one_time_pickup, 
-                                balance = balance, suspend_start = suspend_start, suspend_end = suspend_end, user = user)
+        # suspend_start = request.POST.get('suspend_start')
+        # suspend_end = request.POST.get('suspend_end')
+        new_customer = Customer(name = name, address = address, zipcode = zipcode, weekly_pickup_day = weekly_pickup_day, 
+                                balance = balance, user = user)
         new_customer.save()
         return HttpResponseRedirect(reverse('customers:index'))
     else:
