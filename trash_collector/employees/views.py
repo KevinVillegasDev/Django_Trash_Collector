c10d41e99d451e61d02c5598647edbc3fa40f74f
unknown <kaciehawkins1@gmail.com>
2021-08-04 14:42:36 -0600

    created create method in views and added path in urls to create

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponseRedirect
+from django.urls.base import reverse
+from trash_collector.employees.models import Employee
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.apps import apps
@@ -11,3 +14,15 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+def create(request):
+    user = request.user
+    if request.method == "POST":
+        name = request.POST.get("name")
+        zipcode = request.POST.get("zipcode")
+        new_employee = Employee(name=name,zipcode = zipcode, user = user)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index')) 
+    else: 
+        return render(request, 'employees/create.html')
+        
